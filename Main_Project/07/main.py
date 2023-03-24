cfa86c50191e1a3ca013c467fc4a2f18f1f88df2
pp <1409688109@qq.com>
2023-03-24 08:22:20 +0800

    pp230324

--- Main_Project/07/main.py
+++ Main_Project/07/main.py
@@ -20,11 +20,11 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'本次程序运行的设备环境为{device}，{torch.cuda.get_device_name(device)}')
 
 # 设定工作模式
-training_or_testing = int(input("请输入模式选择，0代表训练，1代表只作测试："))  # 0:训练 1:测试
-if training_or_testing == 0:
-    print("本次程序将会进行训练，并测试模型")
-else:
-    print("本次程序只作测试")
+# training_or_testing = int(input("请输入模式选择，0代表训练，1代表只作测试："))  # 0:训练 1:测试
+# if training_or_testing == 0:
+#     print("本次程序将会进行训练，并测试模型")
+# else:
+#     print("本次程序只作测试")
 
 # 可调参数
 size_basic = 128
@@ -36,10 +36,14 @@ size_decoder_input = size_basic
 size_decoder_hidden_lstm = size_basic
 size_decoder_hidden_fc = size_basic
 size_decoder_output_fc = Ql * Qw
+size_transition_input = Ql * Qw
+size_transition_hidden_fc = size_basic
+size_transition_output_fc = size_decoder_input
+
 size_K = 4
 size_delta = training_data_output.shape[1]
-learning_rate = 1
-max_epochs = 500
+learning_rate = 1e-5
+max_epochs = 10000
 
 # 更改数据类型
 training_data_input = torch.from_numpy(training_data_input).to(torch.float32).to(device)
@@ -54,8 +58,8 @@ print(f'testing_data_output: {testing_data_output.shape}')
 
 # 编码器
 class Encoder(nn.Module):
-    def __int__(self, encoder_input_size, encoder_hidden_size_fc, encoder_hidden_size_lstm, encoder_output_size_lstm, ):
-        super(Encoder, self).__int__()
+    def __init__(self, encoder_input_size, encoder_hidden_size_fc, encoder_hidden_size_lstm, encoder_output_size_lstm):
+        super(Encoder, self).__init__()
         self.encoder_hidden_size_lstm = encoder_hidden_size_lstm
         self.encoder_fc1 = nn.Linear(encoder_input_size, encoder_hidden_size_fc)
         self.encoder_fc2 = nn.Linear(encoder_hidden_size_fc, encoder_hidden_size_fc)
@@ -67,21 +71,20 @@ class Encoder(nn.Module):
         h0 = torch.zeros(1, x.size(0), self.encoder_hidden_size_lstm).to(device)
         c0 = torch.zeros(1, x.size(0), self.encoder_hidden_size_lstm).to(device)
 
-        encoded = self.encoder_fc1(x)
-        encoded = self.encoder_fc2(encoded)
-        encoded = self.encoder_fc3(encoded)
-        encoded, (h1, c1) = self.encoder_lstm1(encoded, (h0, c0))
-        encoded, (h2, c2) = self.encoder_lstm2(encoded, (h0, c0))
-        encoded = encoded[:, -1, :].unsqueeze(1)
+        y = self.encoder_fc1(x)
+        y = self.encoder_fc2(y)
+        y = self.encoder_fc3(y)
+        y, (h1, c1) = self.encoder_lstm1(y, (h0, c0))
+        y, (h2, c2) = self.encoder_lstm2(y, (h0, c0))
+        y = y[:, -1, :].unsqueeze(1)
 
-        return encoded, (h1, c1), (h2, c2)
+        return y, (h1, c1), (h2, c2)
 
 
 # 解码器
 class Decoder(nn.Module):
-    def __int__(self, decoder_input_size, decoder_hidden_size_fc, decoder_hidden_size_lstm, decoder_output_size_fc,
-                decoder_k, decoder_qw, decoder_ql, decoder_delta):
-        super(Decoder, self).__int__()
+    def __init__(self, decoder_input_size, decoder_hidden_size_fc, decoder_hidden_size_lstm, decoder_output_size_fc):
+        super(Decoder, self).__init__()
         self.decoder_lstm1 = nn.LSTM(decoder_input_size, decoder_hidden_size_lstm, num_layers=1, batch_first=True)
         self.decoder_lstm2 = nn.LSTM(decoder_hidden_size_lstm, decoder_hidden_size_lstm, num_layers=1, batch_first=True)
         self.decoder_fc1 = nn.Linear(decoder_hidden_size_lstm, decoder_hidden_size_fc)
@@ -91,13 +94,6 @@ class Decoder(nn.Module):
         self.decoder_fc4 = nn.Linear(decoder_output_size_fc, decoder_input_size)
         self.decoder_fc5 = nn.Linear(decoder_input_size, decoder_input_size)
 
-        self.decoder_fc_ql = nn.Linear(decoder_ql, int(decoder_input_size / 2))
-        self.decoder_fc_qw = nn.Linear(decoder_qw, int(decoder_input_size / 2))
-        self.decoder_K = decoder_k
-        self.decoder_Qw = decoder_qw
-        self.decoder_Ql = decoder_ql
-        self.decoder_delta = decoder_delta
-
     def decoder(self, x, h1, c1, h2, c2):
         y, (h1, c1) = self.decoder_lstm1(x, (h1, c1))
         y, (h2, c2) = self.decoder_lstm2(y, (h2, c2))
@@ -105,14 +101,22 @@ class Decoder(nn.Module):
         y = self.decoder_fc2(y)
         y = self.decoder_fc3(y)
         y = self.decoder_softmax(y)
+        y = y.squeeze(1)
 
         return y, (h1, c1), (h2, c2)
 
 
 # 过渡器
 class Transition(nn.Module):
-    def __int__(self):
-        super(Transition, self).__int__()
+    def __init__(self, transition_input_size, transition_hidden_size_fc, transition_output_size_fc):
+        super(Transition, self).__init__()
+        self.transition_fc1 = nn.Linear(transition_input_size, transition_hidden_size_fc)
+        self.transition_fc2 = nn.Linear(transition_hidden_size_fc, transition_output_size_fc)
+
+    def transition(self, x):
+        y = self.transition_fc1(x)
+        y = self.transition_fc2(y)
+        return y
 
 
 # 整合编码器和解码器
@@ -187,28 +191,31 @@ class EncoderDecoder(nn.Module):
 
 
 # 设置模型基本参数
-model_predict = EncoderDecoder(size_encoder_input, size_encoder_hidden_fc, size_encoder_hidden_lstm,
-                               size_encoder_output_lstm,
-                               size_decoder_input, size_decoder_hidden_fc, size_decoder_hidden_lstm,
-                               size_decoder_output_fc,
-                               size_K, Qw, Ql, size_delta).to(device)
-optimizer = torch.optim.Adam(model_predict.parameters(), lr=learning_rate)
-criterion = nn.L1Loss()
+encoder = Encoder(size_encoder_input, size_encoder_hidden_fc, size_encoder_hidden_lstm, size_encoder_output_lstm).to(device)
+decoder = Decoder(size_decoder_input, size_decoder_hidden_fc, size_decoder_hidden_lstm, size_decoder_output_fc).to(device)
+transition = Transition(size_transition_input, size_transition_hidden_fc, size_transition_output_fc)
 
-if training_or_testing == 0:
-    for epoch in range(max_epochs):
-        _, trajectory_prediction = model_predict.forward(training_data_input)
-        loss = criterion(trajectory_prediction, torch.zeros(
-            [trajectory_prediction.shape[0], trajectory_prediction.shape[1], trajectory_prediction.shape[2]]).to(
-            device))
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+optimizer_encoder = torch.optim.Adam(encoder.parameters(), lr=learning_rate)
+optimizer_decoder = torch.optim.Adam(decoder.parameters(), lr=learning_rate)
+optimizer_transition = torch.optim.Adam(transition.parameters(), lr=learning_rate)
 
-        if (epoch + 1) % 10 == 0:
-            print(loss.item())
-            tensorboard_writer.add_scalar("loss", loss.item(), epoch)
+criterion = nn.CrossEntropyLoss()
+# 单点训练
+for epoch in range(max_epochs):
+    encoded, (h1, c1), (h2, c2) = encoder.encoder(training_data_input)
+    decoded, (h1, c1), (h2, c2) = decoder.decoder(encoded, h1, c1, h2, c2)
+    loss = criterion(decoded, training_data_output)
+    optimizer_encoder.zero_grad()
+    optimizer_decoder.zero_grad()
+    loss.backward()
+    if (epoch+1) % 50 == 0:
+        print(f"epoch:{epoch+1},loss:{loss.item()}")
+        tensorboard_writer.add_scalar("loss", loss.item(), epoch)
+    optimizer_encoder.step()
+    optimizer_decoder.step()
 
-    tensorboard_writer.close()
-else:
-    print("程序结束")
+
+
+
+
+
--- Main_Project/07/utils.py
+++ Main_Project/07/utils.py
@@ -4,6 +4,14 @@ import torch.nn as nn
 import torch
 import matplotlib.pyplot as plt
 
+Qw = 15
+Ql = 25
+start_x = 20
+start_y = 10
+grid_w = 0.1  # y
+grid_l = 0.1  # x
+print(f"Qw * Ql = {Qw * grid_w * Ql * grid_l}m^2")
+
 # stamp x y Vx Vy
 file_name = 'source.txt'
 file_data = open(file_name, 'r')
@@ -19,45 +27,46 @@ for row in file_data_lines:
     data[temp, 4] = float(find_data[4])
     temp += 1
 
-
 batch_size = 10
 hidden_length = 5
-jump_size = batch_size * 2
+jump_size = int(batch_size * 1)
 split_size = 1
-# # 滑窗重组
-# data_reshape = np.array([[0, 0.0, 0.0, 0.0, 0.0]])
-# for i in range(0, data.shape[0] - jump_size, jump_size):
-#     for j in range(0, batch_size*split_size, split_size):
-#         data_reshape[-1, :] = np.array(data[i+j, :])
-#         data_reshape = np.r_[data_reshape, np.array([[0, 0.0, 0.0, 0.0, 0.0]])]
-# np.save("data_reshape.npy", data_reshape)
+# 滑窗重组
+data_reshape = np.array([[0, 0.0, 0.0, 0.0, 0.0]])
+for i in range(0, data.shape[0] - jump_size, jump_size):
+    for j in range(0, batch_size*split_size, split_size):
+        data_reshape[-1, :] = np.array(data[i+j, :])
+        data_reshape = np.r_[data_reshape, np.array([[0, 0.0, 0.0, 0.0, 0.0]])]
+np.save("data_reshape.npy", data_reshape)
 
 # 加载滑窗重组的结果
 data_reshape = np.load("data_reshape.npy")
 data_reshape = np.delete(data_reshape, [-1], 0)
 data_reshape = np.array(data_reshape.reshape(-1, batch_size, 5))
 dis = np.zeros(data_reshape.shape[0])
-print(data_reshape.shape)
+print(f"data_reshape:{data_reshape.shape}")
 
 # plt.figure()
 # for i in range(dis.shape[0]):
 #     dis[i] = math.sqrt(
 #         (data_reshape[i, 0, 1] - data_reshape[i, -1, 1]) ** 2 + (data_reshape[i, 0, 2] - data_reshape[i, -1, 2]) ** 2)
-    # plt.clf()
-    # plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 2], "*", label='trajectory')
-    # # plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 3], label='Vx')
-    # # plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 4], label='Vy')
-    # plt.legend(loc='upper right')
-    # plt.pause(0.001)
-    # print(f"{dis[i]:.4f}")
+# plt.clf()
+# plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 2], "*", label='trajectory')
+# # plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 3], label='Vx')
+# # plt.plot(data_reshape[i, :, 1], data_reshape[i, :, 4], label='Vy')
+# plt.legend(loc='upper right')
+# plt.pause(0.001)
+# print(f"{dis[i]:.4f}")
 # print(dis.max())
 
 rate = 0.5
 training_data_input = np.array(data_reshape[0:int(data_reshape.shape[0] * rate), :, :])
 testing_data_input = np.array(
     data_reshape[int(data_reshape.shape[0] * rate):(data_reshape.shape[0] - data_reshape.shape[1]), :, :])
-training_data_output = np.zeros([training_data_input.shape[0], training_data_input.shape[1], 3])
-testing_data_output = np.zeros([testing_data_input.shape[0], testing_data_input.shape[1], 3])
+# training_data_output = np.zeros([training_data_input.shape[0], training_data_input.shape[1], 3])
+# testing_data_output = np.zeros([testing_data_input.shape[0], testing_data_input.shape[1], 3])
+training_data_output = np.zeros([training_data_input.shape[0], 1, Ql * Qw])
+testing_data_output = np.zeros([testing_data_input.shape[0], 1, Ql * Qw])
 
 for i in range(training_data_output.shape[0]):
     for j in range(training_data_output.shape[1]):
@@ -74,30 +83,24 @@ for i in range(testing_data_output.shape[0]):
         testing_data_output[i, j, 2] = data[int(testing_data_output[i, j, 0] - 1), 2] - testing_data_input[i, 0, 2]
     testing_data_input[i, :, 1] = testing_data_input[i, :, 1] - testing_data_input[i, 0, 1]
     testing_data_input[i, :, 2] = testing_data_input[i, :, 2] - testing_data_input[i, 0, 2]
-training_data_output = training_data_output[:, :, 1:3]
-testing_data_output = testing_data_output[:, :, 1:3]
+# training_data_output = training_data_output[:, :, 1:3]
+# testing_data_output = testing_data_output[:, :, 1:3]
 
 # 栅格化,观测车辆在最初点的左下（5m，5m）的位置，车辆原点在Qw*Ql这个矩形的最下边中心
-Qw = 15
-Ql = 25
-start_x = 20
-start_y = 10
-grid_w = 0.1  # y
-grid_l = 0.1  # x
-print(f"Qw * Ql = {Qw * grid_w * Ql * grid_l}m^2")
-
 training_data_input[:, :, 1] = training_data_input[:, :, 1] // grid_l + start_x
 training_data_input[:, :, 2] = training_data_input[:, :, 2] // grid_w + start_y
-training_data_output[:, :, 0] = training_data_output[:, :, 0] // grid_l + start_x
-training_data_output[:, :, 1] = training_data_output[:, :, 1] // grid_w + start_y
+# training_data_output[:, :, 0] = training_data_output[:, :, 0] // grid_l + start_x
+# training_data_output[:, :, 1] = training_data_output[:, :, 1] // grid_w + start_y
+training_data_output[:, :, 1] = training_data_output[:, :, 1] // grid_l + start_x
+training_data_output[:, :, 2] = training_data_output[:, :, 2] // grid_w + start_y
 
 testing_data_input[:, :, 1] = testing_data_input[:, :, 1] // grid_l + start_x
 testing_data_input[:, :, 2] = testing_data_input[:, :, 2] // grid_w + start_y
-testing_data_output[:, :, 0] = testing_data_output[:, :, 0] // grid_l + start_x
-testing_data_output[:, :, 1] = testing_data_output[:, :, 1] // grid_w + start_y
+# testing_data_output[:, :, 0] = testing_data_output[:, :, 0] // grid_l + start_x
+# testing_data_output[:, :, 1] = testing_data_output[:, :, 1] // grid_w + start_y
+testing_data_output[:, :, 0] = testing_data_output[:, :, 1] // grid_l + start_x
+testing_data_output[:, :, 1] = testing_data_output[:, :, 2] // grid_w + start_y
 
-# training_data_output = np.squeeze(training_data_output, axis=2)
-# testing_data_output = np.squeeze(testing_data_output, axis=2)
 
 # print(training_data_input[:, :, 1].min(), training_data_input[:, :, 2].min())
 # print(training_data_output[:, :, 0].min(), training_data_output[:, :, 1].min())
@@ -116,6 +119,26 @@ testing_data_output[:, :, 1] = testing_data_output[:, :, 1] // grid_w + start_y
 #     plt.plot(testing_data_input[i, :, 1], testing_data_input[i, :, 2], "*")
 #     plt.plot(testing_data_output[i, :, 0, 0], testing_data_output[i, :, 0, 1], ".")
 #     plt.pause(0.001)
+
+for i in range(training_data_output.shape[0]):
+    for j in range(training_data_output.shape[1]):
+        l = training_data_output[i, j, 1]
+        w = training_data_output[i, j, 2]
+        training_data_output[i, j, 0:3] = 0
+        flag = int((w - 1) * Qw + l)
+        training_data_output[i, j, flag] = 1
+
+for i in range(testing_data_output.shape[0]):
+    for j in range(testing_data_output.shape[1]):
+        l = testing_data_output[i, j, 1]
+        w = testing_data_output[i, j, 2]
+        testing_data_output[i, j, 0:3] = 0
+        flag = int((w - 1) * Qw + l)
+
+        testing_data_output[i, j, flag] = 1
+
+training_data_output = np.squeeze(training_data_output, axis=1)
+testing_data_output = np.squeeze(testing_data_output, axis=1)
 
 def criterion(predicted, actual):
     middle = torch.zeros([predicted.shape[0], predicted.shape[1], 1, predicted.shape[3]]).to(torch.device('cuda:0'))
