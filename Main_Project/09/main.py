b9d68ac98644e57b2de803cbc972a1b75d59cac8
pp <1409688109@qq.com>
2023-04-05 23:06:40 +0800

    pp

---
+++ Main_Project/09/main.py
@@ -0,0 +1,253 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from utils import training_data_input, training_data_output, testing_data_input, testing_data_output
+from utils import data_size, input_size, row, column, side_length_y, side_length_x_center, side_length_x
+from utils import training_data_output_origin, testing_data_output_origin
+import matplotlib.pyplot as plt
+import torch.optim.lr_scheduler as scheduler
+
+# 清空缓存，固定随即种子
+torch.manual_seed(1)
+torch.cuda.empty_cache()
+
+# 设置运行设备的环境为GPU
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(f'本次程序运行的设备环境为{device}，{torch.cuda.get_device_name(device)}')
+
+# 更改数据类型
+training_data_input = torch.from_numpy(training_data_input).to(torch.float32).to(device)
+training_data_output = torch.from_numpy(training_data_output).to(torch.float32).to(device)
+testing_data_input = torch.from_numpy(testing_data_input).to(torch.float32).to(device)
+testing_data_output = torch.from_numpy(testing_data_output).to(torch.float32).to(device)
+
+print(f'training_data_input: {training_data_input.shape}')
+print(f'training_data_output: {training_data_output.shape}')
+print(f'testing_data_input: {testing_data_input.shape}')
+print(f'testing_data_output: {testing_data_output.shape}')
+
+# 定义基本参数
+size_basic = 128
+size_encoder_fc_input = data_size - 1  # 减去index
+size_encoder_fc_middle = size_basic
+size_encoder_fc_output = size_basic
+size_encoder_lstm_input = size_encoder_fc_output
+size_encoder_lstm_hidden = size_basic
+size_encoder_activate_num_parameters = input_size
+
+size_decoder_lstm_input = size_encoder_lstm_hidden
+size_decoder_lstm_hidden = size_basic
+size_decoder_fc_input = size_decoder_lstm_hidden
+size_decoder_fc_middle = size_basic
+size_decoder_fc_output = int(row * column)
+
+learning_rate_init = 1e-4
+learning_rate = learning_rate_init
+max_epoch = 100
+
+
+# 定义编码器
+class Encoder(nn.Module):
+    def __init__(self, encoder_fc_input_size, encoder_fc_middle_size, encoder_fc_output_size,
+                 encoder_lstm_input_size, encoder_lstm_hidden_size,
+                 encoder_activate_num_parameters_size):
+        super(Encoder, self).__init__()
+        self.encoder_activate_init = 1.0
+        self.encoder_lstm_hidden_size = encoder_lstm_hidden_size
+        self.encoder_bias = True
+        self.encoder_lstm_num_layers = 2
+
+        self.encoder_fc1 = nn.Linear(encoder_fc_input_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc2 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc3 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc4 = nn.Linear(encoder_fc_middle_size, encoder_fc_output_size, bias=self.encoder_bias)
+        self.encoder_lstm_front = nn.LSTM(encoder_lstm_input_size, encoder_lstm_hidden_size,
+                                          num_layers=self.encoder_lstm_num_layers, batch_first=True)
+        self.encoder_lstm_back = nn.LSTM(encoder_lstm_input_size, encoder_lstm_hidden_size,
+                                         num_layers=self.encoder_lstm_num_layers, batch_first=True)
+        self.encoder_activate1 = nn.PReLU(num_parameters=encoder_activate_num_parameters_size,
+                                          init=self.encoder_activate_init)
+        self.encoder_activate2 = nn.PReLU(num_parameters=encoder_activate_num_parameters_size,
+                                          init=self.encoder_activate_init)
+        self.encoder_activate3 = nn.PReLU(num_parameters=encoder_activate_num_parameters_size,
+                                          init=self.encoder_activate_init)
+
+    def forward(self, x):
+        h0 = torch.ones(self.encoder_lstm_num_layers, x.size(0), self.encoder_lstm_hidden_size).to(device)
+        c0 = torch.ones(self.encoder_lstm_num_layers, x.size(0), self.encoder_lstm_hidden_size).to(device)
+        h1 = torch.ones(self.encoder_lstm_num_layers, x.size(0), self.encoder_lstm_hidden_size).to(device)
+        c1 = torch.ones(self.encoder_lstm_num_layers, x.size(0), self.encoder_lstm_hidden_size).to(device)
+
+        out = self.encoder_fc1(x)
+        out = self.encoder_fc2(out)
+        out = self.encoder_fc3(out)
+        out = self.encoder_fc4(out)
+        out_front, (h_front, c_front) = self.encoder_lstm_front(out, (h0, c0))
+        out_back, (h_back, c_back) = self.encoder_lstm_back(out.flip(dims=[1]), (h1, c1))
+        h = torch.add(h_front, h_back)
+        c = torch.add(c_front, c_back)
+        out = torch.add(out_front, out_back)
+        out = out[:, -1, :].unsqueeze(1)
+        return out, (h, c)
+
+
+# 定义解码器
+class Decoder(nn.Module):
+    def __init__(self, decoder_lstm_input_size, decoder_lstm_hidden_size,
+                 decoder_fc_input_size, decoder_fc_middle_size, decoder_fc_output_size):
+        super(Decoder, self).__init__()
+        self.decoder_lstm_hidden_size = decoder_lstm_hidden_size
+        self.decoder_bias = True
+        self.decoder_activate_init = 1.0
+        self.decoder_lstm_num_layers = 2
+
+        self.decoder_lstm = nn.LSTM(decoder_lstm_input_size, decoder_lstm_hidden_size,
+                                    num_layers=self.decoder_lstm_num_layers, batch_first=True)
+        self.decoder_fc1 = nn.Linear(decoder_fc_input_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc2 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc3 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc4 = nn.Linear(decoder_fc_middle_size, decoder_fc_output_size, bias=self.decoder_bias)
+        self.decoder_softmax = nn.Softmax(dim=2)
+        self.decoder_activate1 = nn.PReLU(num_parameters=1,
+                                          init=self.decoder_activate_init)
+        self.decoder_activate2 = nn.PReLU(num_parameters=1,
+                                          init=self.decoder_activate_init)
+        self.decoder_activate3 = nn.PReLU(num_parameters=1,
+                                          init=self.decoder_activate_init)
+
+    def forward(self, x, h1, c1):
+        out, (h2, c2) = self.decoder_lstm(x, (h1, c1))
+        out = self.decoder_fc1(out)
+        out = self.decoder_fc2(out)
+        out = self.decoder_fc3(out)
+        out = self.decoder_fc4(out)
+        out = self.decoder_softmax(out)
+        return out, (h2, c2)
+
+
+# 模型实例化
+encoder = Encoder(size_encoder_fc_input, size_encoder_fc_middle, size_encoder_fc_output,
+                  size_encoder_lstm_input, size_encoder_lstm_hidden, size_encoder_activate_num_parameters).to(device)
+decoder = Decoder(size_decoder_lstm_input, size_decoder_lstm_hidden,
+                  size_decoder_fc_input, size_decoder_fc_middle, size_decoder_fc_output).to(device)
+
+# 优化器和损失函数
+optimizer_encoder = optim.Adam(encoder.parameters(), lr=learning_rate)
+optimizer_decoder = optim.Adam(decoder.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss()
+
+# 模式选取
+mode_switch = int(input("请进行模式选择："))
+
+# 主要部分
+fig = plt.figure()
+if mode_switch == 0:
+    print("进行模型训练")
+
+    scheduler_encoder = scheduler.StepLR(optimizer_encoder, step_size=100, gamma=0.85, last_epoch=-1)
+    scheduler_decoder = scheduler.StepLR(optimizer_decoder, step_size=100, gamma=0.85, last_epoch=-1)
+
+    all_loss = np.zeros([1])
+    for epoch in range(max_epoch):
+        encoded, (h_encoded, c_encoded) = encoder(training_data_input)
+        decoded, _ = decoder(encoded, h_encoded, c_encoded)
+        loss = criterion(decoded[:, 0, :].clone(), training_data_output[:, 0, :])
+
+        plt.clf()
+        show = 150
+        cal = 100
+
+        all_loss[epoch] = loss.item()
+        plt.plot(all_loss[max([epoch - show, 0]):(epoch + 1)])
+        plt.plot([show, show], [all_loss[max([epoch - show, 0]):(epoch + 1)].min(),
+                                all_loss[max([epoch - show, 0]):(epoch + 1)].max()], "r--")
+        plt.plot([show - cal, show - cal], [all_loss[max([epoch - show, 0]):(epoch + 1)].min(),
+                                            all_loss[max([epoch - show, 0]):(epoch + 1)].max()], "r--")
+        plt.text(show / 2, (all_loss[max([epoch - show, 0]):(epoch + 1)].max() +
+                            all_loss[max([epoch - show, 0]):(epoch + 1)].min()) / 2,
+                 f"lr:{learning_rate:.10f}", fontsize=10)
+
+        all_loss = np.append(all_loss, [0.0], axis=0)
+
+        plt.pause(0.001)
+
+        optimizer_encoder.zero_grad()
+        optimizer_decoder.zero_grad()
+        loss.backward()
+
+        if (epoch + 1) % 50 == 0:
+            print(f"epoch:{epoch + 1},loss:{loss.item()}")
+
+        optimizer_encoder.step()
+        optimizer_decoder.step()
+
+        scheduler_encoder.step()
+        scheduler_decoder.step()
+        learning_rate = scheduler_encoder.get_last_lr()[0]
+
+        rand_para = torch.randperm(training_data_input.shape[0])
+        training_data_input = training_data_input[rand_para]
+        training_data_output = training_data_output[rand_para]
+
+    torch.save(encoder, "end_encoder.pth")
+    torch.save(decoder, "end_decoder.pth")
+    fig.savefig("single.png")
+if mode_switch == 1:
+    print("模型测试")
+
+    encoder = torch.load("end_encoder.pth")
+    decoder = torch.load("end_decoder.pth")
+
+    check_input = training_data_input
+    check_output = training_data_output
+    check_output_origin = training_data_output_origin
+
+    encoded, (h_encoded, c_encoded) = encoder(check_input)
+    decoded, _ = decoder(encoded, h_encoded, c_encoded)
+    loss = criterion(decoded[:, 0, :], check_output[:, 0, :]) * check_output.shape[0]
+    print(loss.item())
+
+    check_input = check_input.cpu().detach().numpy()
+    check_output = check_output.cpu().detach().numpy()
+    decoded = decoded.cpu().detach().numpy()
+
+    for i in range(check_output.shape[0]):
+        plt.clf()
+        plt.plot([0.0, 0.0], [-1.0, 1.0], "r--")
+        lim = 5
+        plt.xlim(-lim, lim)
+        plt.ylim(-lim, lim)
+        plt.plot(check_input[i, :, 0], check_input[i, :, 1], ".")
+        plt.plot(check_output_origin[i, :, 1], check_output_origin[i, :, 2], ".")
+
+        lim = 8
+        plt.xlim(-lim, lim)
+        plt.ylim(0.0, row * side_length_y)
+        plt.plot([side_length_x_center / 2, side_length_x_center / 2], [0.0, side_length_y * 3], "r--")
+        plt.plot([-side_length_x_center / 2, -side_length_x_center / 2], [0.0, side_length_y * 3], "r--")
+        plt.plot([(side_length_x_center + side_length_x / 2), -(side_length_x_center + side_length_x / 2)],
+                 [side_length_y, side_length_y], "r--")
+        plt.plot([(side_length_x_center + side_length_x / 2), -(side_length_x_center + side_length_x / 2)],
+                 [side_length_y * 2, side_length_y * 2], "r--")
+        plt.text(-(side_length_x + side_length_x_center) / 2, side_length_y / 2,
+                 decoded[i, 0, 0], fontsize=10)
+        plt.text(0.0, side_length_y / 2,
+                 decoded[i, 0, 1], fontsize=10)
+        plt.text((side_length_x + side_length_x_center) / 2, side_length_y / 2,
+                 decoded[i, 0, 2], fontsize=10)
+        plt.text(-(side_length_x + side_length_x_center) / 2, side_length_y + side_length_y / 2,
+                 decoded[i, 0, 3], fontsize=10)
+        plt.text(0.0, side_length_y + side_length_y / 2,
+                 decoded[i, 0, 4], fontsize=10)
+        plt.text((side_length_x + side_length_x_center) / 2, side_length_y + side_length_y / 2,
+                 decoded[i, 0, 5], fontsize=10)
+        plt.text(-(side_length_x + side_length_x_center) / 2, 2 * side_length_y + side_length_y / 2,
+                 decoded[i, 0, 6], fontsize=10)
+        plt.text(0.0, 2 * side_length_y + side_length_y / 2,
+                 decoded[i, 0, 7], fontsize=10)
+        plt.text((side_length_x + side_length_x_center) / 2, 2 * side_length_y + side_length_y / 2,
+                 decoded[i, 0, 8], fontsize=10)
+        plt.pause(0.01)
+
+
---
+++ Main_Project/09/utils.py
@@ -0,0 +1,291 @@
+import math
+import numpy as np
+import torch.nn as nn
+import torch
+import matplotlib.pyplot as plt
+
+# stamp x y Vx Vy
+file_name = 'source.txt'
+file_data = open(file_name, 'r')
+file_data_lines = file_data.readlines()
+data_size = 3
+data = np.zeros((len(file_data_lines), data_size))
+temp = 0
+for row in file_data_lines:
+    find_data = row.split(' ')
+    data[temp, 0] = float(find_data[0])
+    data[temp, 1] = float(find_data[1])
+    data[temp, 2] = float(find_data[2])
+    temp += 1
+
+seq_size = 50
+jump_size = int(seq_size / 2)
+split_size = 10
+# # 滑窗重组
+# data_reshape = np.array([[0, 0.0, 0.0]])
+# for i in range(0, data.shape[0] - jump_size - seq_size*split_size, jump_size):
+#     for j in range(0, seq_size*split_size, split_size):
+#         data_reshape[-1, :] = np.array(data[i+j, :])
+#         data_reshape = np.r_[data_reshape, np.array([[0, 0.0, 0.0]])]
+# np.save("data_reshape.npy", data_reshape)
+
+# 加载滑窗重组的结果
+data_reshape = np.load("data_reshape.npy")
+data_reshape = np.delete(data_reshape, [-1], 0)
+data_reshape = np.array(data_reshape.reshape(-1, seq_size, data_size))
+
+# 设置边界格，两行三列，1m*1m
+side_length_x = 2
+side_length_x_center = 3
+side_length_y = 4
+row = 3
+column = 3
+
+# 分割input和output
+input_size = int(0.5 * seq_size)
+output_size = seq_size - input_size
+train_size = int(0.5 * data_reshape.shape[0])
+test_size = data_reshape.shape[0] - train_size
+
+training_data_input = np.array(data_reshape[0:train_size, 0:input_size, :])
+# training_data_output_origin = np.array(data_reshape[0:train_size, (input_size + 1):(input_size + 2), :])
+training_data_output_origin = np.array(data_reshape[0:train_size, input_size:data_reshape.shape[1], :])
+
+testing_data_input = np.array(data_reshape[train_size:data_reshape.shape[0], 0:input_size, :])
+# testing_data_output_origin = np.array(data_reshape[train_size:data_reshape.shape[0], (input_size + 1):(input_size + 2), :])
+testing_data_output_origin = np.array(
+    data_reshape[train_size:data_reshape.shape[0], input_size:data_reshape.shape[1], :])
+
+# 采用相对坐标，以训练集最后一个点为原点
+for i in range(training_data_input.shape[0]):
+    for j in range(training_data_output_origin.shape[1]):
+        training_data_output_origin[i, j, 1] = training_data_output_origin[i, j, 1] - training_data_input[i, -1, 1]
+        training_data_output_origin[i, j, 2] = training_data_output_origin[i, j, 2] - training_data_input[i, -1, 2]
+    for j in range(training_data_input.shape[1]):
+        training_data_input[i, j, 1] = training_data_input[i, j, 1] - training_data_input[i, -1, 1]
+        training_data_input[i, j, 2] = training_data_input[i, j, 2] - training_data_input[i, -1, 2]
+
+for i in range(testing_data_input.shape[0]):
+    for j in range(testing_data_output_origin.shape[1]):
+        testing_data_output_origin[i, j, 1] = testing_data_output_origin[i, j, 1] - testing_data_input[i, -1, 1]
+        testing_data_output_origin[i, j, 2] = testing_data_output_origin[i, j, 2] - testing_data_input[i, -1, 2]
+    for j in range(testing_data_input.shape[1]):
+        testing_data_input[i, j, 1] = testing_data_input[i, j, 1] - testing_data_input[i, -1, 1]
+        testing_data_input[i, j, 2] = testing_data_input[i, j, 2] - testing_data_input[i, -1, 2]
+
+# 旋转数据组
+for i in range(training_data_input.shape[0]):
+    theda = math.atan2(training_data_input[i, -5, 2],
+                       training_data_input[i, -5, 1])  # 与x轴的夹角
+    theda = 3 * math.pi / 2 - theda
+
+    for j in range(training_data_input.shape[1]):
+        x_temp = training_data_input[i, j, 1]
+        y_temp = training_data_input[i, j, 2]
+        training_data_input[i, j, 1] = x_temp * math.cos(theda) - y_temp * math.sin(theda)
+        training_data_input[i, j, 2] = x_temp * math.sin(theda) + y_temp * math.cos(theda)
+
+    for j in range(training_data_output_origin.shape[1]):
+        x_temp = training_data_output_origin[i, j, 1]
+        y_temp = training_data_output_origin[i, j, 2]
+        training_data_output_origin[i, j, 1] = x_temp * math.cos(theda) - y_temp * math.sin(theda)
+        training_data_output_origin[i, j, 2] = x_temp * math.sin(theda) + y_temp * math.cos(theda)
+
+for i in range(testing_data_input.shape[0]):
+    theda = math.atan2(testing_data_input[i, -5, 2],
+                       testing_data_input[i, -5, 1])  # 与x轴的夹角
+    theda = 3 * math.pi / 2 - theda
+
+    for j in range(testing_data_input.shape[1]):
+        x_temp = testing_data_input[i, j, 1]
+        y_temp = testing_data_input[i, j, 2]
+        testing_data_input[i, j, 1] = x_temp * math.cos(theda) - y_temp * math.sin(theda)
+        testing_data_input[i, j, 2] = x_temp * math.sin(theda) + y_temp * math.cos(theda)
+
+    for j in range(testing_data_output_origin.shape[1]):
+        x_temp = testing_data_output_origin[i, j, 1]
+        y_temp = testing_data_output_origin[i, j, 2]
+        testing_data_output_origin[i, j, 1] = x_temp * math.cos(theda) - y_temp * math.sin(theda)
+        testing_data_output_origin[i, j, 2] = x_temp * math.sin(theda) + y_temp * math.cos(theda)
+
+# 分类
+# 6 7 8
+# 3 4 5
+# 0 1 2
+training_data_output = np.zeros([training_data_output_origin.shape[0],
+                                 1,
+                                 row * column])
+testing_data_output = np.zeros([testing_data_output_origin.shape[0],
+                                1,
+                                row * column])
+for i in range(training_data_output.shape[0]):
+    for j in range(training_data_output_origin.shape[1]):
+        if abs(training_data_output_origin[i, j, 1]) <= (side_length_x_center / 2):
+            # 在1，4，7内
+            if training_data_output_origin[i, j, 2] <= side_length_y:
+                # 在1内
+                training_data_output[i, 0, 1] = training_data_output[i, 0, 1] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在4内
+                training_data_output[i, 0, 4] = training_data_output[i, 0, 4] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在7内
+                training_data_output[i, 0, 7] = training_data_output[i, 0, 7] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}，y = {training_data_output_origin[i, j, 2]}]")
+                raise IndexError
+        elif (training_data_output_origin[i, j, 1] > side_length_x_center / 2) and \
+             (training_data_output_origin[i, j, 1] <= (side_length_x_center + side_length_x)):
+            # 在2，5，8内
+            if training_data_output_origin[i, j, 2] <= side_length_y:
+                # 在2内
+                training_data_output[i, 0, 2] = training_data_output[i, 0, 2] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在5内
+                training_data_output[i, 0, 5] = training_data_output[i, 0, 5] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在8内
+                training_data_output[i, 0, 8] = training_data_output[i, 0, 8] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}]，y = {training_data_output_origin[i, j, 2]}")
+                raise IndexError
+        elif (training_data_output_origin[i, j, 1] > -(side_length_x_center + side_length_x)) and \
+             (training_data_output_origin[i, j, 1] <= -(side_length_x_center / 2)):
+            # 在0，3，6内
+            if training_data_output_origin[i, j, 2] <= side_length_y:
+                # 在0内
+                training_data_output[i, 0, 0] = training_data_output[i, 0, 0] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在3内
+                training_data_output[i, 0, 3] = training_data_output[i, 0, 3] + 1
+            elif (training_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (training_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在6内
+                training_data_output[i, 0, 6] = training_data_output[i, 0, 6] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}]，y = {training_data_output_origin[i, j, 2]}")
+                raise IndexError
+        else:
+            print(f"超出范围，请增大范围，当前i={i}，x = {training_data_output_origin[i, j, 1]}")
+            raise IndexError
+    training_data_output[i, 0, :] = training_data_output[i, 0, :] / training_data_output[i, 0, :].sum()
+
+for i in range(testing_data_output.shape[0]):
+    for j in range(testing_data_output_origin.shape[1]):
+        if abs(testing_data_output_origin[i, j, 1]) <= (side_length_x_center / 2):
+            # 在1，4，7内
+            if testing_data_output_origin[i, j, 2] <= side_length_y:
+                # 在1内
+                testing_data_output[i, 0, 1] = testing_data_output[i, 0, 1] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在4内
+                testing_data_output[i, 0, 4] = testing_data_output[i, 0, 4] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在7内
+                testing_data_output[i, 0, 7] = testing_data_output[i, 0, 7] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}，y = {testing_data_output_origin[i, j, 2]}]")
+                raise IndexError
+        elif (testing_data_output_origin[i, j, 1] > side_length_x_center / 2) and \
+             (testing_data_output_origin[i, j, 1] <= (side_length_x_center + side_length_x)):
+            # 在2，5，8内
+            if testing_data_output_origin[i, j, 2] <= side_length_y:
+                # 在2内
+                testing_data_output[i, 0, 2] = testing_data_output[i, 0, 2] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在5内
+                testing_data_output[i, 0, 5] = testing_data_output[i, 0, 5] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在8内
+                testing_data_output[i, 0, 8] = testing_data_output[i, 0, 8] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}]，y = {testing_data_output_origin[i, j, 2]}")
+                raise IndexError
+        elif (testing_data_output_origin[i, j, 1] > -(side_length_x_center + side_length_x)) and \
+             (testing_data_output_origin[i, j, 1] <= -(side_length_x_center / 2)):
+            # 在0，3，6内
+            if testing_data_output_origin[i, j, 2] <= side_length_y:
+                # 在0内
+                testing_data_output[i, 0, 0] = testing_data_output[i, 0, 0] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 2):
+                # 在3内
+                testing_data_output[i, 0, 3] = testing_data_output[i, 0, 3] + 1
+            elif (testing_data_output_origin[i, j, 2] > side_length_y * 2) and \
+                 (testing_data_output_origin[i, j, 2] <= side_length_y * 3):
+                # 在6内
+                testing_data_output[i, 0, 6] = testing_data_output[i, 0, 6] + 1
+            else:
+                print(f"超出范围，请增大范围，当前[{i}, {j}]，y = {testing_data_output_origin[i, j, 2]}")
+                raise IndexError
+        else:
+            print(f"超出范围，请增大范围，当前i={i}，x = {testing_data_output_origin[i, j, 1]}")
+            raise IndexError
+    testing_data_output[i, 0, :] = testing_data_output[i, 0, :] / testing_data_output[i, 0, :].sum()
+
+
+# plt.figure()
+# for i in range(training_data_input.shape[0]):
+#     plt.clf()
+#     plt.plot([0.0, 0.0], [-1.0, 1.0], "r--")
+#     lim = 5
+#     plt.xlim(-lim, lim)
+#     plt.ylim(-lim, lim)
+#     plt.plot(training_data_input[i, :, 1], training_data_input[i, :, 2], ".")
+#     plt.plot(training_data_output_origin[i, :, 1], training_data_output_origin[i, :, 2], ".")
+#
+#     lim = 8
+#     plt.xlim(-lim, lim)
+#     plt.ylim(0.0, row * side_length_y)
+#     plt.plot([side_length_x_center / 2, side_length_x_center / 2], [0.0, side_length_y * 3], "r--")
+#     plt.plot([-side_length_x_center / 2, -side_length_x_center / 2], [0.0, side_length_y * 3], "r--")
+#     plt.plot([(side_length_x_center + side_length_x / 2), -(side_length_x_center + side_length_x / 2)],
+#              [side_length_y, side_length_y], "r--")
+#     plt.plot([(side_length_x_center + side_length_x / 2), -(side_length_x_center + side_length_x / 2)],
+#              [side_length_y * 2, side_length_y * 2], "r--")
+#     plt.text(-(side_length_x + side_length_x_center) / 2, side_length_y / 2,
+#              training_data_output[i, 0, 0], fontsize=10)
+#     plt.text(0.0, side_length_y / 2,
+#              training_data_output[i, 0, 1], fontsize=10)
+#     plt.text((side_length_x + side_length_x_center) / 2, side_length_y / 2,
+#              training_data_output[i, 0, 2], fontsize=10)
+#     plt.text(-(side_length_x + side_length_x_center) / 2, side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 3], fontsize=10)
+#     plt.text(0.0, side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 4], fontsize=10)
+#     plt.text((side_length_x + side_length_x_center) / 2, side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 5], fontsize=10)
+#     plt.text(-(side_length_x + side_length_x_center) / 2, 2 * side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 6], fontsize=10)
+#     plt.text(0.0, 2 * side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 7], fontsize=10)
+#     plt.text((side_length_x + side_length_x_center) / 2, 2 * side_length_y + side_length_y / 2,
+#              training_data_output[i, 0, 8], fontsize=10)
+#
+#     # plt.plot([training_data_input[i, -1, 1], training_data_input[i, -2, 1]],
+#     #          [training_data_input[i, -1, 2], training_data_input[i, -2, 2]],
+#     #          "r--")
+#     # plt.plot(training_data_input[i, -2:training_data_input.shape[1], 1],
+#     #          training_data_input[i, -2:training_data_input.shape[1], 2], ".")
+#     # if i == 100:
+#     #     print()
+#     # theda = math.atan2(training_data_input[i, -5, 2],
+#     #                    training_data_input[i, -5, 1])
+#     # print(theda * 180 / math.pi)
+#     plt.pause(0.001)
+
+
+# # 移除index参数
+training_data_input = np.array(training_data_input[:, :, 1:data_size])
+# training_data_output = np.array(training_data_output_origin[:, :, 1:3])
+testing_data_input = np.array(testing_data_input[:, :, 1:data_size])
+# testing_data_output = np.array(testing_data_output_origin[:, :, 1:3])
