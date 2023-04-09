eb173249d9dc209282e7e21e1818aaa25e6dc1ef
BILL-LIU301 <1409688109@qq.com>
2023-04-09 15:39:03 +0800

    1216

--- Main_Project/09/main.py
+++ Main_Project/09/main.py
@@ -8,6 +8,19 @@ from utils import training_data_output_origin, testing_data_output_origin
 import matplotlib.pyplot as plt
 import torch.optim.lr_scheduler as scheduler
 
+# 启动前检查
+print('CUDA版本:', torch.version.cuda)
+print('Pytorch版本:', torch.__version__)
+print('显卡是否可用:', '可用' if (torch.cuda.is_available()) else '不可用')
+print('显卡数量:', torch.cuda.device_count())
+print('是否支持BF16数字格式:', '支持' if (torch.cuda.is_bf16_supported()) else '不支持')
+print('当前显卡型号:', torch.cuda.get_device_name())
+print('当前显卡的CUDA算力:', torch.cuda.get_device_capability())
+print('当前显卡的总显存:', torch.cuda.get_device_properties(0).total_memory / 1024 / 1024 / 1024, 'GB')
+print('是否支持TensorCore:', '支持' if (torch.cuda.get_device_properties(0).major >= 7) else '不支持')
+print('当前显卡的显存使用率:', torch.cuda.memory_allocated(0) / torch.cuda.get_device_properties(0).total_memory * 100,
+      '%')
+
 # 清空缓存，固定随即种子
 torch.manual_seed(1)
 torch.cuda.empty_cache()
@@ -28,7 +41,7 @@ print(f'testing_data_input: {testing_data_input.shape}')
 print(f'testing_data_output: {testing_data_output.shape}')
 
 # 定义基本参数
-size_basic = 256
+size_basic = 512
 size_encoder_fc_input = data_size - 1  # 减去index
 size_encoder_fc_middle = size_basic
 size_encoder_fc_output = size_basic
@@ -53,19 +66,24 @@ class Encoder(nn.Module):
                  encoder_lstm_input_size, encoder_lstm_hidden_size,
                  encoder_activate_num_parameters_size):
         super(Encoder, self).__init__()
-        self.encoder_activate_init = 1.0
+        self.encoder_activate_init = 0.05
         self.encoder_lstm_hidden_size = encoder_lstm_hidden_size
         self.encoder_bias = True
-        self.encoder_lstm_num_layers = 2
+        self.encoder_lstm_num_layers = 1
 
         self.encoder_fc1 = nn.Linear(encoder_fc_input_size, encoder_fc_middle_size, bias=self.encoder_bias)
         self.encoder_fc2 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
         self.encoder_fc3 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
-        self.encoder_fc4 = nn.Linear(encoder_fc_middle_size, encoder_fc_output_size, bias=self.encoder_bias)
+        self.encoder_fc4 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc5 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc6 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc7 = nn.Linear(encoder_fc_middle_size, encoder_fc_middle_size, bias=self.encoder_bias)
+        self.encoder_fc8 = nn.Linear(encoder_fc_middle_size, encoder_fc_output_size, bias=self.encoder_bias)
         self.encoder_lstm_front = nn.LSTM(encoder_lstm_input_size, encoder_lstm_hidden_size,
                                           num_layers=self.encoder_lstm_num_layers, batch_first=True)
         self.encoder_lstm_back = nn.LSTM(encoder_lstm_input_size, encoder_lstm_hidden_size,
                                          num_layers=self.encoder_lstm_num_layers, batch_first=True)
+        self.encoder_activate = nn.LeakyReLU(negative_slope=0.1)
         self.encoder_activate1 = nn.PReLU(num_parameters=encoder_activate_num_parameters_size,
                                           init=self.encoder_activate_init)
         self.encoder_activate2 = nn.PReLU(num_parameters=encoder_activate_num_parameters_size,
@@ -80,16 +98,20 @@ class Encoder(nn.Module):
         c1 = torch.ones(self.encoder_lstm_num_layers, x.size(0), self.encoder_lstm_hidden_size).to(device)
 
         out = self.encoder_fc1(x)
-        out = self.encoder_fc2(self.encoder_activate1(out))
-        out = self.encoder_fc3(self.encoder_activate2(out))
-        out = self.encoder_fc4(self.encoder_activate3(out))
-        out_front, (h_front, c_front) = self.encoder_lstm_front(out, (h0, c0))
-        out_back, (h_back, c_back) = self.encoder_lstm_back(out.flip(dims=[1]), (h1, c1))
-        h = torch.add(h_front, h_back)
-        c = torch.add(c_front, c_back)
+        out_res = out.clone()
+        out = self.encoder_fc2(self.encoder_activate(out))
+        out = self.encoder_fc3(self.encoder_activate(out))
+        out = self.encoder_fc4(self.encoder_activate(torch.add(out_res, out)))
+        out_res = out.clone()
+        out = self.encoder_fc5(self.encoder_activate(out))
+        out = self.encoder_fc6(self.encoder_activate(out))
+        out = self.encoder_fc7(self.encoder_activate(torch.add(out_res, out)))
+        out = self.encoder_fc8(self.encoder_activate(out))
+        out_front, _ = self.encoder_lstm_front(out, (h0, c0))
+        out_back, _ = self.encoder_lstm_back(out.flip(dims=[1]), (h1, c1))
         out = torch.add(out_front, out_back)
         out = out[:, -1, :].unsqueeze(1)
-        return out, (h, c)
+        return out
 
 
 # 定义解码器
@@ -99,16 +121,21 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self.decoder_lstm_hidden_size = decoder_lstm_hidden_size
         self.decoder_bias = True
-        self.decoder_activate_init = 1.0
-        self.decoder_lstm_num_layers = 2
+        self.decoder_activate_init = 0.05
+        self.decoder_lstm_num_layers = 1
 
         self.decoder_lstm = nn.LSTM(decoder_lstm_input_size, decoder_lstm_hidden_size,
                                     num_layers=self.decoder_lstm_num_layers, batch_first=True)
         self.decoder_fc1 = nn.Linear(decoder_fc_input_size, decoder_fc_middle_size, bias=self.decoder_bias)
         self.decoder_fc2 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
         self.decoder_fc3 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
-        self.decoder_fc4 = nn.Linear(decoder_fc_middle_size, decoder_fc_output_size, bias=self.decoder_bias)
+        self.decoder_fc4 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc5 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc6 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc7 = nn.Linear(decoder_fc_middle_size, decoder_fc_middle_size, bias=self.decoder_bias)
+        self.decoder_fc8 = nn.Linear(decoder_fc_middle_size, decoder_fc_output_size, bias=self.decoder_bias)
         self.decoder_softmax = nn.Softmax(dim=2)
+        self.decoder_activate = nn.LeakyReLU(negative_slope=0.1)
         self.decoder_activate1 = nn.PReLU(num_parameters=1,
                                           init=self.decoder_activate_init)
         self.decoder_activate2 = nn.PReLU(num_parameters=1,
@@ -118,14 +145,18 @@ class Decoder(nn.Module):
         self.decoder_activate4 = nn.PReLU(num_parameters=1,
                                           init=self.decoder_activate_init)
 
-    def forward(self, x, h1, c1):
-        out, (h2, c2) = self.decoder_lstm(x, (h1, c1))
-        out = self.decoder_fc1(self.decoder_activate1(out))
-        out = self.decoder_fc2(self.decoder_activate2(out))
-        out = self.decoder_fc3(self.decoder_activate3(out))
-        out = self.decoder_fc4(self.decoder_activate4(out))
-        out = self.decoder_softmax(out)
-        return out, (h2, c2)
+    def forward(self, x):
+        out = self.decoder_fc1(self.decoder_activate(x))
+        out_res = out.clone()
+        out = self.decoder_fc2(self.decoder_activate(out))
+        out = self.decoder_fc3(self.decoder_activate(out))
+        out = self.decoder_fc4(self.decoder_activate(torch.add(out_res, out)))
+        out_res = out.clone()
+        out = self.decoder_fc5(self.decoder_activate(out))
+        out = self.decoder_fc6(self.decoder_activate(out))
+        out = self.decoder_fc7(self.decoder_activate(torch.add(out_res, out)))
+        out = self.decoder_fc8(self.decoder_activate(out))
+        return out
 
 
 # 模型实例化
@@ -133,10 +164,13 @@ encoder = Encoder(size_encoder_fc_input, size_encoder_fc_middle, size_encoder_fc
                   size_encoder_lstm_input, size_encoder_lstm_hidden, size_encoder_activate_num_parameters).to(device)
 decoder = Decoder(size_decoder_lstm_input, size_decoder_lstm_hidden,
                   size_decoder_fc_input, size_decoder_fc_middle, size_decoder_fc_output).to(device)
+softmax = nn.Softmax(dim=2)
+print('当前显卡的显存使用率:',
+      torch.cuda.memory_allocated(0) / torch.cuda.get_device_properties(0).total_memory * 100, '%')
 
 # 优化器和损失函数
-optimizer_encoder = optim.SGD(encoder.parameters(), lr=learning_rate, weight_decay=0.5)
-optimizer_decoder = optim.SGD(decoder.parameters(), lr=learning_rate, weight_decay=0.5)
+optimizer_encoder = optim.Adam(encoder.parameters(), lr=learning_rate)
+optimizer_decoder = optim.Adam(decoder.parameters(), lr=learning_rate)
 criterion = nn.CrossEntropyLoss()
 
 # 模式选取
@@ -147,17 +181,20 @@ fig = plt.figure()
 if mode_switch == 0:
     print("进行模型训练")
 
-    scheduler_encoder = scheduler.StepLR(optimizer_encoder, step_size=200, gamma=0.9, last_epoch=-1)
-    scheduler_decoder = scheduler.StepLR(optimizer_decoder, step_size=200, gamma=0.9, last_epoch=-1)
+    scheduler_encoder = scheduler.StepLR(optimizer_encoder, step_size=100, gamma=0.9, last_epoch=-1)
+    scheduler_decoder = scheduler.StepLR(optimizer_decoder, step_size=100, gamma=0.9, last_epoch=-1)
 
     all_loss = np.zeros([1])
     for epoch in range(max_epoch):
-        encoded, (h_encoded, c_encoded) = encoder(training_data_input)
-        decoded, _ = decoder(encoded, h_encoded, c_encoded)
-        loss = criterion(decoded[:, 0, :].clone(), training_data_output[:, 0, :]) * testing_data_output.shape[0]
+        encoded = encoder(training_data_input)
+        decoded = decoder(encoded)
+        decoded = softmax(decoded)
+        loss = criterion(decoded[:, 0, :], training_data_output[:, 0, :])
+        # print('当前显卡的显存使用率:',
+        #       torch.cuda.memory_allocated(0) / torch.cuda.get_device_properties(0).total_memory * 100, '%')
 
         plt.clf()
-        show = 150
+        show = 200
         cal = 100
 
         all_loss[epoch] = loss.item()
@@ -205,8 +242,8 @@ if mode_switch == 1:
     check_output = testing_data_output
     check_output_origin = training_data_output_origin
 
-    encoded, (h_encoded, c_encoded) = encoder(check_input)
-    decoded, _ = decoder(encoded, h_encoded, c_encoded)
+    encoded = encoder(check_input)
+    decoded = decoder(encoded)
     loss = criterion(decoded[:, 0, :], check_output[:, 0, :]) * check_output.shape[0]
     print(loss.item())
 
